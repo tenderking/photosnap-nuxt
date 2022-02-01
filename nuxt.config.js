68da15c169a8e757d1691dd00446b2ee0176c486
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2022-02-01 17:01:15 +0000

    chore: add FEM config

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Photosnap',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'A Frontend Mentor Challenge' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/png', size: '32x32', href: '/favicon-32x32.png' }],
+    link: [{ rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    link: [{ rel: 'preconnect', href: 'https://fonts.gstatic.com' }],
+    link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Navbar />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
