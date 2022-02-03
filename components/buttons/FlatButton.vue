59ea8e5fa7b6b0313aa8004f93cff619c91f305a
tenderking <gmu003@gmail.com>
2022-02-03 01:59:45 +0100

    feat: created flat-button

--- components/buttons/FlatButton.vue
+++ components/buttons/FlatButton.vue
@@ -1,3 +1,44 @@
 <template>
-  <p>Hi</p>
+  <button class="btn flat-button">
+    <span class="flat-button__content font-h4">
+      <slot>{{ message }}</slot>
+    </span>
+    <svg xmlns="http://www.w3.org/2000/svg" width="43" height="14">
+      <g fill="none" fill-rule="evenodd" stroke="#000">
+        <path d="M0 7h41.864M35.428 1l6 6-6 6" id="target" />
+      </g>
+    </svg>
+  </button>
 </template>
+
+<script>
+export default {
+  name: 'FlatButton',
+  props: {
+    message: {
+      // It can be either one or several types for one attribute
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.flat-button {
+  display: flex;
+  gap: 22px;
+  &_dark-text {
+    color: $clr-black;
+    #target {
+      stroke: $clr-black;
+    }
+  }
+  &_light-text {
+    color: $clr-white;
+    #target {
+      stroke: $clr-white;
+    }
+  }
+}
+</style>
