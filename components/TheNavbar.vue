f4bb26bdbc2b7c35ff4048f6b16042759517181a
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2022-02-05 12:19:37 +0000

    style: finish the design of the navbar

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -2,15 +2,33 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'Navbar',
+  name: 'TheNavbar',
   data() {
     return {
-      isOpen: false,
+      isOpen: null,
+      isMobile: null,
     }
   },
   methods: {
     toggleNavbar() {
       this.isOpen = !this.isOpen
+    },
+    checkScreen() {
+      if (process.browser) {
+        const screenWidth = window.innerWidth
+
+        this.isMobile = screenWidth < 768
+        this.isOpen = screenWidth > 768
+      }
+    },
+  },
+  created() {
+    this.checkScreen()
+    if (process.browser) window.addEventListener('resize', this.checkScreen)
+  },
+  watch: {
+    isMobile() {
+      if (this.isMobile) this.isOpen = true
     },
   },
 })
@@ -35,7 +53,7 @@ export default Vue.extend({
       <ButtonsNormalButton :isDark="true">Get an invite</ButtonsNormalButton>
     </div>
 
-    <div class="navbar__menu">
+    <div class="navbar__menu" v-show="isMobile">
       <button class="btn btn--icon" v-if="!isOpen" @click="toggleNavbar">
         <img src="/shared/mobile/menu.svg" alt="Open the navbar" />
       </button>
@@ -89,6 +107,36 @@ export default Vue.extend({
 
     .btn {
       width: 100%;
+    }
+
+    @media (min-width: $tablet) {
+      position: static;
+      padding: 0;
+      width: 66.6%; /* 2/3 of 100% */
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .link-list {
+        flex-direction: row;
+        margin: 0;
+
+        &::before {
+          position: initial;
+        }
+
+        * + * {
+          margin-left: 1rem;
+        }
+
+        &__item {
+          padding: 0;
+        }
+      }
+
+      .btn {
+        width: initial;
+      }
     }
   }
 }
